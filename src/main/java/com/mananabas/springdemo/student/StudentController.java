efe25f3d3b469076ceaa7bf21fc8a92f40d72365
juicy <juicylemon3@gmail.com>
2023-07-30 01:28:41 +0800

    init

---
+++ src/main/java/com/mananabas/springdemo/student/StudentController.java
@@ -0,0 +1,50 @@
+package com.mananabas.springdemo.student;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+
+    private StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Student save(
+            @RequestBody Student student
+    ){
+        return service.save(student);
+    }
+
+    @GetMapping("/{email}")
+    public Student findByEmail(
+           @PathVariable("email") String email
+    ){
+        return service.findByEmail(email);
+    }
+
+    @GetMapping
+    public List<Student> findAllStudents() {
+        return service.findAllStudents();
+    }
+
+    @PutMapping
+    public Student updateStudent(
+            @RequestBody Student student
+    ){
+        return service.update(student);
+    }
+
+    @DeleteMapping("/{email,}")
+    public void delete(
+            @PathVariable("email") String email
+    ){
+        service.delete(email);
+    }
+}
---
+++ src/test/java/com/mananabas/springdemo/SpringBootBegineerHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mananabas.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBegineerHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
